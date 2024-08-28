1fadafe7e520b23ecdc0dba3465a9570d5eb2c77
thuyh0h <trongthuynl2004@gmail.com>
2024-08-28 23:10:54 +0700

    stm32f103xx_flash_driver.h .c is completed

--- stm32f10x/Drivers/include/stm32f103xx_dma_driver.h
+++ stm32f10x/Drivers/include/stm32f103xx_dma_driver.h
@@ -1,10 +1,10 @@
-#ifndef __DMA_H
-#define __DMA_H
+#ifndef __STM32F103XX_DMA_DRIVER_H_
+#define __STM32F103XX_DMA_DRIVER_H_
 
-#include "stm32f10x.h"
+#include "stm32f103xx.h"
 #include "stdint.h"
 
-__WEAK void DMA_Setup(DMA_Channel_TypeDef *DMA_Channel);// function settup in main.c file
-void DMA_Transfer(DMA_Channel_TypeDef *DMA_Channel, volatile void* Mem_ptr, volatile void* Peri_ptr, uint32_t lenght);
+//void DMA_Setup(DMA_Channel_TypeDef *DMA_Channel);// function settup in main.c file
+//void DMA_Transfer(DMA_Channel_TypeDef *DMA_Channel, volatile void* Mem_ptr, volatile void* Peri_ptr, uint32_t lenght);
 
-#endif
+#endif /* __STM32F103XX_DMA_DRIVER_H_ */
